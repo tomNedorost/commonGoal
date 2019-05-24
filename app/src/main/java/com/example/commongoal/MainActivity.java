f8ad4a433a6092a523af945d688303044bbce129
tomNedorost <tom.nrost@gmail.com>
2019-05-24 13:27:32 +0200

    init commit

---
+++ app/src/main/java/com/example/commongoal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.commongoal;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // init commit
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
